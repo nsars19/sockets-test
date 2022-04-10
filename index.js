1748ba4e50366240ce9753f2b066e20dabcd2e39
Nick Sardonini <nsardonini@umassp.edu>
2022-04-10 13:08:56 -0400

    add user disconnect logic

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.get("/user-leaving", (req, res) => {
+  id.on("connection", () => {
+    io.emit("user disconnect", { id: req.query.id, name: req.query.name });
+  });
+
+  res.send(200);
+});
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
@@ -32,11 +40,12 @@ io.on("connection", (socket) => {
     io.emit("mousemove", { id, name, x, y });
   });
 
-  socket.on("disconnect", ({ id }) => {
+  socket.on("user disconnect", ({ id, name }) => {
+    console.log("user disconnected: ", id, name);
     io.emit("user disconnect", { id });
   });
 });
 
 server.listen(process.env.PORT || 3002, () => {
-  console.log("listening on *:" + process.env.PORT || 3002);
+  console.log("listening on *:" + (process.env.PORT ?? 3002));
 });
